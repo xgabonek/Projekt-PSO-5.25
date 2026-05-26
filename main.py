92cde9bafa58702f655e0e34a6e803f9b48780f4
xgabonek <gabonminecraft@gmail.com>
2026-05-26 14:31:11 +0200

    Wersja 1.5 - Main Menu dostało dwie nowe funkcje, w tym jedna już działa

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def main_menu():
         def __init__(self):
             super().__init__()
             self.setWindowTitle("Diablo II")
-            self.setStyleSheet("background-image: url('tlo_menu.jpg'); color: white;")
 
             # QMainWindow wymaga centralnego widgetu cosik jak taki container
             centralny = QWidget()
@@ -18,17 +17,34 @@ def main_menu():
 
             layout = QVBoxLayout()
             centralny.setLayout(layout)
+            centralny.setObjectName("centralny")
+            centralny.setStyleSheet("QWidget#centralny { background-image: url('tlo_menu.jpg'); }")
 
             # Logo
-            tytul = QLabel()
-            tytul_img = QPixmap('diablo_logo.png')
-            tytul.setPixmap(tytul_img)
-            tytul.setStyleSheet("background: none;")
+            tytul = QLabel("Diablo II")
+            tytul.setStyleSheet("background: none; letter-spacing: 10px; color: white; font-size: 48px;")
             tytul.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            
 
             # Przyciski
-            btn_style = "background-color: #333; padding: 20px; font-size: 18px;"
-            font_id = QFontDatabase.addApplicationFont("Outfit-bold.ttf")
+            btn_style = btn_style = (
+                "QPushButton{"
+                "background-color: rgba(36, 33, 35, 153);"
+                "padding: 20px;"
+                "font-size: 18px;"
+                "letter-spacing: 2px;"
+                "color: white;"
+                "}"
+                "QPushButton:hover{"
+                "background-color: rgba(36, 33, 35, 204);"
+                "}"
+                "QPushButton:pressed{"
+                "background-color: rgba(36, 33, 35, 242);"
+                "color: rgb(220, 220, 220);"
+                "}"
+            )
+            
+            font_id = QFontDatabase.addApplicationFont("font_bold.ttf")
             families = QFontDatabase.applicationFontFamilies(font_id)
 
             single = QPushButton("Singleplayer")
@@ -39,8 +55,23 @@ def main_menu():
             multi.setStyleSheet(btn_style)
             multi.setFixedWidth(500)
 
+            settings = QPushButton("Ustawienia")
+            settings.setStyleSheet(btn_style)
+            settings.setFixedWidth(500)
+
+            exit = QPushButton("Wyjdź z Diablo II")
+            exit.setStyleSheet(btn_style)
+            exit.setFixedWidth(500)
+            exit.clicked.connect(sys.exit)
+
+
             single.setFont(QFont(families[0], 24))
             multi.setFont(QFont(families[0], 24))
+            tytul.setFont(QFont(families[0], 36))
+            settings.setFont(QFont(families[0], 24))
+            exit.setFont(QFont(families[0], 24))
+
+
 
             # The layout of doom and despair
             layout.addStretch()
@@ -48,9 +79,12 @@ def main_menu():
             layout.addSpacing(50)
             layout.addWidget(single, alignment=Qt.AlignmentFlag.AlignCenter)
             layout.addWidget(multi, alignment=Qt.AlignmentFlag.AlignCenter)
+            layout.addWidget(settings, alignment=Qt.AlignmentFlag.AlignCenter)
+            layout.addWidget(exit, alignment=Qt.AlignmentFlag.AlignCenter)
             layout.addStretch() # To ogolnie mi chmurski powiedzial zeby dodac, to wywala taki nieprzyjemny space pod tymi przyciskami, cholera wie o co z tym chodzi ale trzeba to dodac bo inaczej wybuchnie
 
             self.showFullScreen()
+        
 
     app = QApplication(sys.argv)
     window = MainWindow()
